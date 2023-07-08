8ee1d2c1b8d95900143c40d928d1dbaebe681cfc
Mehmet Ali TAŞ <astronotik31@gmail.com>
2023-07-08 14:59:44 +0300

    Python

---
+++ Python Ders Özeti/10-Modüller/index.py
@@ -0,0 +1,16 @@
+import math # matematik formülleri import edildi
+
+factor = math.factorial(6) # 6 sayısının faktöriyeli alınır
+sqrt = math.sqrt(15)  # karekök fonksiyonu
+fabs = math.fabs(-20) #mutlak değer fonksiyonu
+
+
+from math import factorial #sadece faktöriyel fonksiyonu import edildi
+
+
+
+def cember_cevresi(r): #kendi matematik modülümüzü oluşturup bunu farklı bir dosyada import edebiliriz. Aynı klasörde olmalı
+    return 2 * 3.14 * r
+
+
+
